45fa9fd349644aecc483c7a7d2812c6ef13e0887
ukyo-035 <vocaloidmiku0727@gmail.com>
2026-02-28 23:57:48 +0900

    Add basic script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+let allData = [];
 
+fetch("data.json")
+  .then(res => res.json())
+  .then(data => {
+    allData = data;
+    renderCards(data);
+    updateCount(data.length);
+  });
+
+function renderCards(data) {
+  const container = document.getElementById("cardContainer");
+  container.innerHTML = "";
+
+  data.forEach(item => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <img src="${item.image}">
+      <div>
+        <strong>${item.name}</strong><br>
+        ${item.wave_label} ★${"★".repeat(item.rarity)}
+      </div>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+function updateCount(count) {
+  document.getElementById("resultCount").textContent =
+    `${count}件表示中`;
+}
